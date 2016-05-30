ff4d419eec3bc296da88e62cf5b4f121968ec671
David Love <bungeemonkee@gmail.com>
2016-05-30 17:00:30 -0400

    Set up nuget

--- Transformerizer/Properties/AssemblyInfo.cs
+++ Transformerizer/Properties/AssemblyInfo.cs
@@ -1,30 +1,32 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Transformerizer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Transformerizer: Parallel transform library for .NET")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("David Love")]
 [assembly: AssemblyProduct("Transformerizer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information for an assembly consists of the following four values.
+// We will increase these values in the following way:
+//    Major Version : Increased when there is a release that breaks a public api
+//    Minor Version : Increased for each non-api-breaking release
+//    Build Number : 0 for alpha versions, 1 for beta versions, 2 for release candidates, 3 for releases
+//    Revision : Always 0 for release versions, always 1+ for alpha, beta, rc versions to indicate the alpha/beta/rc number
+[assembly: AssemblyVersion("1.0.2.1")]
+[assembly: AssemblyFileVersion("1.0.2.1")]
+
+// This version number will roughly follow semantic versioning : http://semver.org
+// The first three numbers will always match the first the numbers of the version above.
+[assembly: AssemblyInformationalVersion("1.0.2-rc1")]
