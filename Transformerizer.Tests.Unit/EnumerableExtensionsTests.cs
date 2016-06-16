c759464764ae322658bedad8a9eb29b82daedade
David Love <bungeemonkee@gmail.com>
2016-06-16 11:23:29 -0400

    Adds BeginTransformMany(...) for orthogonality with ThenTransformMany(...)

--- Transformerizer.Tests.Unit/EnumerableExtensionsTests.cs
+++ Transformerizer.Tests.Unit/EnumerableExtensionsTests.cs
@@ -34,5 +34,47 @@ namespace Transformerizer.Tests.Unit
             var enumerable = new BlockingCollection<object>();
             var result = enumerable.BeginTransform(x => x);
         }
+
+        [TestMethod]
+        public void BeginTransformMany_Works_With_General_Enumerable()
+        {
+            var enumerable = new object[0];
+            var result = enumerable.BeginTransformMany(x => new[] { x });
+        }
+
+        [TestMethod]
+        public void BeginTransformMany_Works_With_IProducerConsumer()
+        {
+            var enumerableMock = new Mock<IProducerConsumerCollection<object>>();
+            var result = enumerableMock.Object.BeginTransformMany(x => new[] { x });
+        }
+
+        [TestMethod]
+        public void BeginTransformMany_Works_With_BlockingCollection()
+        {
+            var enumerable = new BlockingCollection<object>();
+            var result = enumerable.BeginTransformMany(x => new[] { x });
+        }
+
+        [TestMethod]
+        public void BeginTransformMany_With_Threads_Works_With_General_Enumerable()
+        {
+            var enumerable = new object[0];
+            var result = enumerable.BeginTransformMany(x => new[] { x }, 1);
+        }
+
+        [TestMethod]
+        public void BeginTransformMany_With_Threads_Works_With_IProducerConsumer()
+        {
+            var enumerableMock = new Mock<IProducerConsumerCollection<object>>();
+            var result = enumerableMock.Object.BeginTransformMany(x => new[] { x }, 1);
+        }
+
+        [TestMethod]
+        public void BeginTransformMany_With_Threads_Works_With_BlockingCollection()
+        {
+            var enumerable = new BlockingCollection<object>();
+            var result = enumerable.BeginTransformMany(x => new[] { x }, 1);
+        }
     }
 }
--- Transformerizer/EnumerableExtensions.cs
+++ Transformerizer/EnumerableExtensions.cs
@@ -52,6 +52,53 @@ namespace Transformerizer
             return new Transformer<TProduce, TConsume>(consume, transform);
         }
 
+        /// <summary>
+        /// Begin a parallel transformation chain by consuming any <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// Manually test several different thread counts for each step in order to optimize your transformation chain.
+        /// Generally IO bound steps should have more threads as they are going to be idle waiting on IO reather than consuming CPU.
+        /// Generally CPU bound steps should have fewer threads as they are going to 
+        /// </remarks>
+        /// <typeparam name="TProduce">The type of item produced by this transformation.</typeparam>
+        /// <typeparam name="TConsume">The type of item consumed by this transformation.</typeparam>
+        /// <param name="source">An enumerable of the initial objects.</param>
+        /// <param name="transform">The delegate that performs the transformations.</param>
+        /// <param name="threads">The number of threads with which to process the transformation.</param>
+        /// <returns>An <see cref="ITransformer{TProduce, TConsume}"/> suitable for parallel processing of transformation steps.</returns>
+        public static ITransformer<TProduce, TConsume> BeginTransformMany<TProduce, TConsume>(this IEnumerable<TConsume> source, TransformMany<TProduce, TConsume> transform, int threads)
+        {
+            // Turn the source into an IProcuderConsumerCollection
+            var consume = CastOrWrap(source);
+
+            // Create a thread-safe producer consumer from this enumerable
+            return new TransformerMany<TProduce, TConsume>(consume, transform, threads);
+        }
+
+        /// <summary>
+        /// Begin a parallel transformation chain by consuming any <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method will use a default number of threads which is almost certainly sub-optimal.
+        /// 
+        /// Manually test several different thread counts for each step in order to optimize your transformation chain.
+        /// Generally IO bound steps should have more threads as they are going to be idle waiting on IO rather than consuming CPU.
+        /// Generally CPU bound steps should have fewer threads as those threads are going to be idle much less.
+        /// </remarks>
+        /// <typeparam name="TProduce">The type of item produced by this transformation.</typeparam>
+        /// <typeparam name="TConsume">The type of item consumed by this transformation.</typeparam>
+        /// <param name="source">An enumerable of the initial objects.</param>
+        /// <param name="transform">The delegate that performs the transformations.</param>
+        /// <returns>An <see cref="ITransformer{TProduce, TConsume}"/> suitable for parallel processing of transformation steps.</returns>
+        public static ITransformer<TProduce, TConsume> BeginTransformMany<TProduce, TConsume>(this IEnumerable<TConsume> source, TransformMany<TProduce, TConsume> transform)
+        {
+            // Turn the source into an IProcuderConsumerCollection
+            var consume = CastOrWrap(source);
+
+            // Create a thread-safe producer consumer from this enumerable
+            return new TransformerMany<TProduce, TConsume>(consume, transform);
+        }
+
         private static IProducerConsumerCollection<T> CastOrWrap<T>(IEnumerable<T> source)
         {
             // If the source already is a producer then use that
